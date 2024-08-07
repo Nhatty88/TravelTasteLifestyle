fc4ae4dde8efc765a88fae94c98d533c0a8b051d
Nhatty88 <nhattynato@gmail.com>
2024-08-08 06:41:02 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 // JavaScript can be added here if needed
+document.getElementById('nav-toggle').addEventListener('click', function() {
+    var menu = document.getElementById('nav-menu');
+    if (menu.classList.contains('show')) {
+        menu.classList.remove('show');
+    } else {
+        menu.classList.add('show');
+    }
+});
