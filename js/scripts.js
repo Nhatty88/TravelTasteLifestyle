6ce9921591f9c0f249f531936b1e202b9cf5c0d4
Nhatty88 <nhattynato@gmail.com>
2024-08-08 06:44:53 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,22 @@ document.getElementById('nav-toggle').addEventListener('click', function() {
         menu.classList.add('show');
     }
 });
+document.getElementById('nav-toggle').addEventListener('click', function() {
+    var menu = document.getElementById('nav-menu');
+    if (menu.classList.contains('show')) {
+        menu.classList.remove('show');
+    } else {
+        menu.classList.add('show');
+    }
+});
+
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    var body = document.body;
+    if (body.classList.contains('light-mode')) {
+        body.classList.remove('light-mode');
+        body.classList.add('dark-mode');
+    } else {
+        body.classList.remove('dark-mode');
+        body.classList.add('light-mode');
+    }
+});
